50b1e0b9e80868bcfdc8e2d4e2338dd5932fc3fd
ricardo betancourt <rb__samael_@hotmail.es>
2023-08-09 22:22:45 -0400

    primer commit base del diseño web

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conscientiamstudios</title>
+
+<!-- #region FONTAWESOME-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- #endregion FONTAWESOME-- -->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+
+
+
+  <link  href="styles/mybootssaas.css" rel="stylesheet" >
+
+  <script src="resources/js/bootstrap.js"></script>
+  <script src="styles/boots.js"></script>
+  
+</head>
+
+
+    
+<body>
+ <div id="back3d">
+
+      <div class="container-fluid" id="vizual">
+         <div class="row">
+
+            <h1 class="">Conscientiamstudios </h1>
+            <nav id="navbar-example2" class="navbar bg-dark px-3 mb-3 rounded-pill">
+               <a class="navbar-brand text-light" href="#">Conscientiamstudios</a>
+               <ul class="nav nav-pills">
+                 <li class="nav-item">
+                   <a class="nav-link" href="#scrollspyHeading1">Home</a>
+                 </li>
+                 <li class="nav-item">
+                   <a class="nav-link" href="#scrollspyHeading2">works</a>
+                 </li>
+           
+            
+                     <li class="nav-item"><a class="nav-link" href="#scrollspyHeading3">members</a></li>
+                     <li class="nav-item"><a class="nav-link" href="#scrollspyHeading4">Projects</a></li>
+        
+                     <li class="nav-item"><a class="nav-link" href="#scrollspyHeading5">Fifth</a></li>
+                     
+     
+
+               </ul>     
+     
+
+          </nav>     
+      <!-- #region SIDEBAR-->
+          <div class="col col-xxl-2">
+
+          <nav  class="d-lg-block sidebar  ">
+            <div class="list-group list-group-flush mx-3 mt-4 text-light ">
+
+              <a href="#" class="list-group-item  py-2 ripple text-light border border-primary rounded-pill bg-dark " >
+                <i class="fas fa-chart-line fa-fw me-3"></i><span class="">Redes Sociales</span>
+              </a>
+              
+              
+              
+                <a href="#" class="fa fa-facebook"></a>
+                 
+                
+               </div>
+             
+          </nav>
+     <!-- #endregion SIDEBAR -->
+
+          </div>
+     
+
+          <div class="col-md-10">
+              <div class="boxsc text-black-50 border border-primary-subtle rounded-3" data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
+               
+                  <!-- #region SUBSECCION-->
+                   <div id="scrollspyHeading1" class="subbox border border-primary-subtle rounded-4"> 
+                     <h4 >First heading</h4>
+                      <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
+                      mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
+                      mucho textomucho textomucho textomucho textomucho texto
+                      </p>
+                 </div>
+                 <!-- #endregion SUBSECCIOPN -->     
+
+              <!-- #region SUBSECCION-->
+                 <div id="scrollspyHeading2" class="subbox border border-primary-subtle rounded-4"> 
+                     <h4 >First heading</h4>
+                      <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
+                      mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
+                      mucho textomucho textomucho textomucho textomucho texto
+                      </p>
+                 </div>
+                 <!-- #endregion SUBSECCIOPN -->
+              <!-- #region SUBSECCION-->
+                  <div id="scrollspyHeading3" class="subbox border border-primary-subtle rounded-4"> 
+                     <h4 >First heading</h4>
+                      <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
+                      mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
+                      mucho textomucho textomucho textomucho textomucho texto
+                      </p>
+                 </div>
+                 <!-- #endregion SUBSECCIOPN -->
+
+                               <!-- #region SUBSECCION-->
+                  <div id="scrollspyHeading4" class="subbox border border-primary-subtle rounded-4"> 
+                     <h4 >First heading</h4>
+                      <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
+                      mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
+                      mucho textomucho textomucho textomucho textomucho texto
+                      </p>
+                 </div>
+                 <!-- #endregion SUBSECCIOPN -->
+           </div>
+        </div>     
+     
+
+         </div><!-- ROW END -->
+      </div>
+
+     <!-- #endregion 3D -->  
+   </div>
+
+   <script async src="https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js"></script>
+
+    <script type="importmap">
+     {
+      "imports": {
+        "three": "https://unpkg.com/three@0.149.0/build/three.module.js",
+        "three/addons/": "https://unpkg.com/three@0.149.0/examples/jsm/"
+      }
+    } 
+    </script>
+   
+   <script type="module" src="resources/js/mainthree.js">
+
+   </script>
+
+
+
+</body>
+</html>
