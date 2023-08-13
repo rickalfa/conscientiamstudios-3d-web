49036ba071ec28b81a780ffefbfa6dd475361ee7
ricardo betancourt <rb__samael_@hotmail.es>
2023-08-13 10:02:46 -0400

    diseño iconos social media

--- index.php
+++ index.php
@@ -12,9 +12,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
 
-
-
-  <link  href="styles/mybootssaas.css" rel="stylesheet" >
+<link  href="styles/mybootssaas.css" rel="stylesheet" >
 
   <script src="resources/js/bootstrap.js"></script>
   <script src="styles/boots.js"></script>
@@ -24,14 +22,36 @@
 
     
 <body>
- <div id="back3d">
+  <!-- #region 3D-->
+ <div id="back3d"></div>
 
-      <div class="container-fluid" id="vizual">
-         <div class="row">
+ <!-- #region VIZUALIZACION-->
+  <div  id="vizual">
 
-            <h1 class="">Conscientiamstudios </h1>
-            <nav id="navbar-example2" class="navbar bg-dark px-3 mb-3 rounded-pill">
-               <a class="navbar-brand text-light" href="#">Conscientiamstudios</a>
+<!--------- #region MAIN CONTAINER-->
+   <div class="container-fluid">
+
+<!-- #region  COllapse Navbar-->
+
+      <div class="collapse " id="navbarToggleExternalContent" data-bs-theme="dark">
+         <div class="nacontent">
+             <div class="p-2">
+              <h5 class="text-body-emphasis h4">Conscientiamstudios dev. indie videogame</h5>
+              
+              <span class="text-body-secondary">dev. assets- unreal engine  app webs.</span>
+              </div>
+       
+          </div>
+      </div>
+
+
+       <nav id="navbar-example2" class="navbar navbar-dark bg-dark rounded-pill">
+         <div class="container-fluid">
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+           </button>
+
+           <a class="navbar-brand text-light" href="#">Conscientiamstudios</a>
                <ul class="nav nav-pills">
                  <li class="nav-item">
                    <a class="nav-link" href="#scrollspyHeading1">Home</a>
@@ -41,90 +61,166 @@
                  </li>
            
             
-                     <li class="nav-item"><a class="nav-link" href="#scrollspyHeading3">members</a></li>
-                     <li class="nav-item"><a class="nav-link" href="#scrollspyHeading4">Projects</a></li>
+                <li class="nav-item"><a class="nav-link" href="#scrollspyHeading3">members</a></li>
+                <li class="nav-item"><a class="nav-link" href="#scrollspyHeading4">Projects</a></li>
         
-                     <li class="nav-item"><a class="nav-link" href="#scrollspyHeading5">Fifth</a></li>
+                <li class="nav-item"><a class="nav-link" href="#scrollspyHeading5">Fifth</a></li>
                      
-     
+               </ul>  
 
-               </ul>     
-     
 
-          </nav>     
-      <!-- #region SIDEBAR-->
-          <div class="col col-xxl-2">
+         </div>
+       </nav>
+ <!-- #endregion COLLAPSE navbar -->
 
-          <nav  class="d-lg-block sidebar  ">
-            <div class="list-group list-group-flush mx-3 mt-4 text-light ">
 
-              <a href="#" class="list-group-item  py-2 ripple text-light border border-primary rounded-pill bg-dark " >
-                <i class="fas fa-chart-line fa-fw me-3"></i><span class="">Redes Sociales</span>
-              </a>
+        <!-- #region 1to RAW -->
+          <div class="row">
+
+ 
+         <h1 class="">Conscientiamstudios </h1>
+
+          <div class="col-lg">
+          <!-- #region BG GREEN-->
+          <div class="bg-success" style="--bs-bg-opacity: .5;"> 
+            
+                  <div class="boxsc text-black-50 border border-primary-subtle rounded-3" data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-2 rounded-2" tabindex="0">
               
-              
-              
-                <a href="#" class="fa fa-facebook"></a>
-                 
-                
-               </div>
+                       <!-- #region SUBSECCION-->
+                      <div id="scrollspyHeading1" class="subbox border border-primary-subtle rounded-4"> 
+                          <h4 >First heading</h4>
+                           <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
+                           mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
+                           mucho textomucho textomucho textomucho textomucho texto
+                           </p>
+                      </div>
+                      <!-- #endregion SUBSECCIOPN -->     
+          
+                   <!-- #region SUBSECCION-->
+                      <div id="scrollspyHeading2" class="subbox border border-primary-subtle rounded-4"> 
+                          <h4 >First heading</h4>
+                           <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
+                           mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
+                           mucho textomucho textomucho textomucho textomucho texto
+                           </p>
+                      </div>
+                      <!-- #endregion SUBSECCIOPN -->
+                   <!-- #region SUBSECCION-->
+                       <div id="scrollspyHeading3" class="subbox border border-primary-subtle rounded-4"> 
+                          <h4 >First heading</h4>
+                           <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
+                           mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
+                           mucho textomucho textomucho textomucho textomucho texto
+                           </p>
+                      </div>
+                      <!-- #endregion SUBSECCIOPN -->
+          
+                                    <!-- #region SUBSECCION-->
+                       <div id="scrollspyHeading4" class="subbox border border-primary-subtle rounded-4"> 
+                          <h4 >First heading</h4>
+                           <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
+                           mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
+                           mucho textomucho textomucho textomucho textomucho texto
+                           </p>
+                      </div>
+                <!-- #endregion SUBSECCIOPN -->
+          
              
-          </nav>
-     <!-- #endregion SIDEBAR -->
-
+               <!-- #endregion CONTENT SUBSECCION -->
+                  </div>
+          
+          
+             </div>
+          <!-- #endregion BG GREEN -->
+          
+          </div>     
+          
+          
           </div>
-     
+        <!-- ROW END -->
+       
+</div>  
 
-          <div class="col-md-10">
-              <div class="boxsc text-black-50 border border-primary-subtle rounded-3" data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example bg-body-tertiary p-3 rounded-2" tabindex="0">
-               
-                  <!-- #region SUBSECCION-->
-                   <div id="scrollspyHeading1" class="subbox border border-primary-subtle rounded-4"> 
-                     <h4 >First heading</h4>
-                      <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
-                      mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
-                      mucho textomucho textomucho textomucho textomucho texto
-                      </p>
-                 </div>
-                 <!-- #endregion SUBSECCIOPN -->     
 
-              <!-- #region SUBSECCION-->
-                 <div id="scrollspyHeading2" class="subbox border border-primary-subtle rounded-4"> 
-                     <h4 >First heading</h4>
-                      <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
-                      mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
-                      mucho textomucho textomucho textomucho textomucho texto
-                      </p>
-                 </div>
-                 <!-- #endregion SUBSECCIOPN -->
-              <!-- #region SUBSECCION-->
-                  <div id="scrollspyHeading3" class="subbox border border-primary-subtle rounded-4"> 
-                     <h4 >First heading</h4>
-                      <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
-                      mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
-                      mucho textomucho textomucho textomucho textomucho texto
-                      </p>
-                 </div>
-                 <!-- #endregion SUBSECCIOPN -->
+<!-- #region  2to CONTAINER -->
+<div class="container">
+        <!-- #region ROW  SOCIAL MEDIA-->
 
-                               <!-- #region SUBSECCION-->
-                  <div id="scrollspyHeading4" class="subbox border border-primary-subtle rounded-4"> 
-                     <h4 >First heading</h4>
-                      <p class="">... mucho texto mucho texto mucho textomucho textomucho textomucho
-                      mucho textomucho textomucho textomucho textomucho texto mucho texto mucho texto mucho textomucho textomucho textomucho
-                      mucho textomucho textomucho textomucho textomucho texto
-                      </p>
-                 </div>
-                 <!-- #endregion SUBSECCIOPN -->
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5 mt-3 ">
+
+           <div class="col mb-3"> </div>
+           <div class="col mb-3"> </div>
+
+            <div class="col-6 mb-0">
+              <div class="d-flex justify-content-center">
+                <a href="#" class="bi bi-twitter border border-bottom-0 rounded-top p-1" style="font-size: 2rem; color: #e664ed;"></a>
+                 
+                <a href="#" class="bi bi-facebook fa rounded-top p-1" style="font-size: 2rem; color: white;"></a>
+                <a href="#" class="bi bi-discord bg-dark fa rounded-top p-1" style="font-size: 2rem; color:#0a65c5;"></a>
+              </div>
+  
+            </div>
+
+           <div class="col mb-0"> 
+        
            </div>
-        </div>     
-     
 
-         </div><!-- ROW END -->
-      </div>
+           <div class="col mb-3"> </div>
+        
 
-     <!-- #endregion 3D -->  
-   </div>
+
+
+        </div>
+
+        <!-- #endregion ROW SOCIAL MEDIA-->
+
+
+        <!-- #region 2to ROW-->
+        
+        <div class="row">
+
+        <!-- #region SOCIAL - Media-->
+        
+   
+
+          <div class="col-lg-12">
+          
+                 <!-- #region FOOTER-->         
+
+                 <footer>
+                   <div class="mt-0 p-2 bg-dark text-white text-center">
+                 
+                      <p>Conscientiamstudios</p>
+
+
+                     
+                     <!-- Copyright -->
+                       <div class="footer-copyright text-center py-3">© 2023 Copyright:
+                        <a href="/"> MDBootstrap.com</a>
+                      </div>
+                      <!-- Copyright -->
+
+
+                    </div>
+                 </footer>
+                  <!-- #endregion FOOTER -->
+
+                
+
+           </div>
+          <!-- #endregion SOCIAL MEDIA -->
+
+
+        </div>
+        <!-- #endregion 2to ROW-->
+  </div>
+    
+       
+    
+   
+  </div>
+  <!-- #endregion VIZUALIZACION-- -->
+
 
    <script async src="https://unpkg.com/es-module-shims@1.6.3/dist/es-module-shims.js"></script>
 
@@ -140,8 +236,6 @@
    <script type="module" src="resources/js/mainthree.js">
 
    </script>
-
-
 
 </body>
 </html>
